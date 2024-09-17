d5a762557b5e10ca26ca6f1b2ac50cfeabaec0c6
Arón Díaz <arondiaz.cpn@gmail.com>
2024-09-17 19:09:12 -0300

    implemented jwt on cookie

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 //Fetch functions
 export const register = async (req, res) => {
@@ -16,14 +17,31 @@ export const register = async (req, res) => {
 
     //created variable userSaved for save all data included the created at and updated at, this information are without the schema model. (timestamps)
     const userSaved = await newUser.save();
+
+    //created token
+    jwt.sign(
+      {
+        id: userSaved._id,
+      },
+      "secret123",
+      {
+        expiresIn: "1d",
+      },
+      (err, token) => {
+        if (err) console.log(err);
+        res.cookie("token", token);
+        res.json({ message: "User created successfully" });
+      }
+    );
+
     //response to show when fetch from frontend
-    res.json({
-      id: userSaved._id,
-      username: userSaved.username,
-      email: userSaved.email,
-      createdAt: userSaved.createdAt,
-      updatedAt: userSaved.updatedAt
-    });
+    // res.json({
+    //   id: userSaved._id,
+    //   username: userSaved.username,
+    //   email: userSaved.email,
+    //   createdAt: userSaved.createdAt,
+    //   updatedAt: userSaved.updatedAt,
+    // });
   } catch (error) {
     console.log(error);
   }
