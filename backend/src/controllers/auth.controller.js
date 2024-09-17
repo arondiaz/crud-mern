0662d764ea19ba8a957f86794acc538484fa8790
Arón Díaz <arondiaz.cpn@gmail.com>
2024-09-17 16:27:14 -0300

    added bcrypt and hash the user password

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,19 +1,29 @@
 import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
 
 //Fetch functions
 export const register = async (req, res) => {
   const { email, password, username } = req.body;
 
   try {
+    const passwordHash = await bcrypt.hash(password, 8);
+
     const newUser = new User({
       username,
       email,
-      password,
+      password: passwordHash,
     });
 
     //created variable userSaved for save all data included the created at and updated at, this information are without the schema model. (timestamps)
     const userSaved = await newUser.save();
-    res.json(userSaved);
+    //response to show when fetch from frontend
+    res.json({
+      id: userSaved._id,
+      username: userSaved.username,
+      email: userSaved.email,
+      createdAt: userSaved.createdAt,
+      updatedAt: userSaved.updatedAt
+    });
   } catch (error) {
     console.log(error);
   }
