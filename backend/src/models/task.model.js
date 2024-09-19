26252102a2c875b0c97d896455f4f7dc6c06975e
Arón Díaz <arondiaz.cpn@gmail.com>
2024-09-19 16:02:20 -0300

    updated task schema for save the author of the task

--- backend/src/models/task.model.js
+++ backend/src/models/task.model.js
@@ -15,6 +15,11 @@ const taskSchema = new mongoose.Schema(
       type: Date,
       default: Date.now,
     },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
   },
   { timestamps: true }
 );
