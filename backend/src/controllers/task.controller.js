a1c856a4918002a5ce70b0e924e8fb004185ed14
Arón Díaz <arondiaz.cpn@gmail.com>
2024-09-19 14:22:45 -0300

    created all routes for task

---
+++ backend/src/controllers/task.controller.js
@@ -0,0 +1,7 @@
+const getTasks = async (req, res) => {};
+
+const getOneTask = async (req, res) => {};
+
+const createTask = async (req, res) => {};
+const updateTask = async (req, res) => {};
+const deleteTask = async (req, res) => {};
--- backend/src/routes/task.routes.js
+++ backend/src/routes/task.routes.js
@@ -1,8 +1,19 @@
 import { Router } from "express";
 import authRequired from "../middlewares/validateToken.js";
+import {
+  getTasks,
+  getOneTask,
+  createTask,
+  deleteTask,
+  updateTask,
+} from "../controllers/task.controller.js";
 
 const router = Router();
 
-router.get("/task", authRequired, (req, res) => res.send("OK task"));
+router.get("/task", authRequired, getTasks);
+router.get("/task/:id", authRequired, getOneTask);
+router.post("/task", authRequired, createTask);
+router.delete("/task/:id", authRequired, deleteTask);
+router.put("/task", authRequired, updateTask);
 
 export default router;
