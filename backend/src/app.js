e05a609f169405778c19bb9e29b532cebca53f1e
Arón Díaz <arondiaz.cpn@gmail.com>
2024-09-20 16:51:07 -0300

    installed and configured cors on backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,9 +3,13 @@ import morgan from "morgan";
 import authRoutes from "./routes/auth.routes.js";
 import cookieParser from "cookie-parser";
 import taskRoutes from "./routes/task.routes.js";
+import cors from "cors"
 
 const app = express();
 
+app.use(cors({
+    origin: "http://localhost:5173"
+}))
 app.use(morgan("dev"));
 app.use(express.json()); //So that express can read the request.body
 app.use(cookieParser());
