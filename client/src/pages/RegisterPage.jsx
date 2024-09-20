70dd28d50811f0c2696520bc823895d0206ef301
Arón Díaz <arondiaz.cpn@gmail.com>
2024-09-20 17:27:08 -0300

    updated register component

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,32 +1,51 @@
-import { useForm } from "react-hook-form";
-import { registerRequest } from "../api/auth";
+import { useState } from "react";
 
 const RegisterPage = () => {
-  const { register, handleSubmit } = useForm();
-
-  const onSubmit = handleSubmit(async (values) => {
-    const res = await registerRequest(values);
-    console.log(res);
+  const [register, setRegister] = useState({
+    username: "",
+    email: "",
+    password: "",
   });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setRegister({
+      ...register,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    console.log(register);
+  };
 
   return (
     <div className="bg-zinc-900 max-w-md p-10 rounded-md flex justify-center items-center h-full">
-      <form onSubmit={onSubmit}>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
-          {...register("username", { required: true })}
+          name="username"
+          value={register.username}
+          onChange={handleInputChange}
+          placeholder="ingresa tu nombre de usuario"
           className="w-full bg-gray-400 text-white px-4 py-2 my-4 rounded-xl"
         />
 
         <input
           type="email"
-          {...register("email", { required: true })}
+          name="email"
+          value={register.email}
+          onChange={handleInputChange}
           className="w-full bg-gray-400 text-white px-4 py-2 my-4 rounded-xl"
         />
 
         <input
           type="password"
-          {...register("password", { required: true })}
+          name="password"
+          value={register.password}
+          onChange={handleInputChange}
           className="w-full bg-gray-400 text-white px-4 py-2 my-4 rounded-xl"
         />
 
